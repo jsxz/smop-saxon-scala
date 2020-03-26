a3f0e412e61a7054e8284487f36b674fc156c4ac
anjsxz <anjsxz@gmail.com>
2020-03-26 23:12:36 +0800

    a

--- src/main/scala/GenPaths.scala
+++ src/main/scala/GenPaths.scala
@@ -1,33 +1,69 @@
 import scala.annotation.tailrec
+import scala.io.Source
 import scala.xml.{Elem, Node, NodeSeq, XML}
 
 object GenPaths {
   def main(args: Array[String]): Unit = {
+    val nodes = Source.fromFile("data/descript.txt").getLines().map {
+      case line => {
+        val strings = line.split(",").toList
+        (strings.head.substring(0, strings.head.length - 8), strings.tail)
+      }
+    }.toMap
+    genParentOne("para", nodes)
+
+  }
+
+  def genParent(li: List[String], nodes: Map[String, List[String]]): List[String] = {
+    li.foreach(s => {
+      if (s != "levelledPara" && s != "content") {
+        println(s)
+        val parentList: List[String] = nodes.filter(n => n._2.contains(s)).map(n => n._1).toList
+        println(parentList)
+        genParent(parentList, nodes)
+      }
+    })
+    List()
+  }
+
+  def genParentOne(s: String, nodes: Map[String, List[String]]): List[String] = {
+    println(s)
+    val parentList: List[String] = nodes.filter(n => n._2.contains(s)).map(n => n._1).toList
+    println(parentList)
+
+    if (parentList.size>0 && s != parentList.head) {
+      parentList.map(s=>genParentOne(s,nodes))
+    }
+
+    List()
+  }
+
+  def genChild(): Unit = {
     val xsd = XML.loadFile("data/4.2/descript.xsd")
     (xsd \\ "complexType").map {
       case e => {
         print((e \@ "name"))
-        getChild(e,xsd)
+        getChild(e, xsd)
         println()
       }
     }
   }
 
-  def getChild(e: Node,xsd:Elem): String = {
+  def getChild(e: Node, xsd: Elem): String = {
     val ref = (e \@ "ref").toString
     if (e.label == "element") {
       print(s",$ref")
     }
     else {
-      if(ref !=""){
-        (xsd \\ "group").filter(t=>(t \@ "name").toString==ref).map{
-          case e1=>{
-            getChild(e1,xsd)
+      if (ref != "") {
+        (xsd \\ "group").filter(t => (t \@ "name").toString == ref).map {
+          case e1 => {
+            getChild(e1, xsd)
           }
         }
       }
       (e \ "_").map {
-        case e1 => getChild(e1,xsd)
+        case e1 => getChild(e1, xsd)
       }
     }
     ""
